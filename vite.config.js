f6825cea57e0e70acf69c7df838545051329589a
Emilio Laurence Dimalanta <2201851@slu.edu.ph>
2023-07-30 23:49:02 +0800

    add new build files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "build",
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "commonjs/[name].js", // Output CommonJS files to commonjs/ directory
+        format: "es",
+      },
+    },
+  },
+});
